12fff27c86b20f5e1e7809fbd8bc11c9a2c7028e
unknown <ghodmareabhishek@gmail.com>
2023-02-22 22:27:48 +0530

    day-1

---
+++ adoreme/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Navbar.module.css"
+
+function Navbar(){
+
+    return (
+        <p className={styles.good}>CALL & SAVE. Save up to $200 on most brands exclusively when you call your Personal Shopper at 888-270-6160</p>
+    )
+}
+
+export default Navbar;
